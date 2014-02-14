3cc46688594f8696b28d1646ae3d62ad1e19c301
jimmy.huangjin <jimmy.huangjin@gmail.com>
2014-02-14 14:23:33 +0800

    update uploader

--- lib/dfs_uploader/dfs_path_helpers.rb
+++ lib/dfs_uploader/dfs_path_helpers.rb
@@ -1,8 +1,8 @@
  # coding: utf-8
 module DfsUploader
 	module DfsPathHelpers
-		
-		def dfs_to_url(dfs, size = nil)
+
+    def dfs_to_url(dfs, size = nil, host = nil)
       arr = (dfs || '').split('|')
       return '' if arr.length <= 1
 
@@ -11,14 +11,32 @@ module DfsUploader
 
       file_path = '/' + File.join(arr[4], arr[-2], arr[-1], file_name)
 
-	    dfs_assets_host = DfsUploader.configuration.assets_host
+      dfs_assets_host = host || DfsUploader.configuration.assets_host
+  
+      if dfs_assets_host.present?
+        file_path = (dfs_assets_host % arr[2]) << file_path
+      end
 
-	    if dfs_assets_host.present?
-	    	file_path = (dfs_assets_host % arr[2]) << file_path
-	    end
+      file_path
+    end
 
-	    file_path
-	  end
+		# def dfs_to_url(dfs, size = nil)
+  #     arr = (dfs || '').split('|')
+  #     return '' if arr.length <= 1
+
+  #     file_name = "#{arr[0]}.#{arr[1]}"
+  #     file_name = "#{size.to_s}_#{file_name}" if size
+
+  #     file_path = '/' + File.join(arr[4], arr[-2], arr[-1], file_name)
+
+	 #    dfs_assets_host = DfsUploader.configuration.assets_host
+
+	 #    if dfs_assets_host.present?
+	 #    	file_path = (dfs_assets_host % arr[2]) << file_path
+	 #    end
+
+	 #    file_path
+	 #  end
 
     def dfs_to_path(dfs, size)
       file_path = get_file_path(dfs, size)
